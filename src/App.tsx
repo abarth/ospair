02de98b108ad39f197eddb5359dab62483b308fc
Adam Barth <github@adambarth.com>
2024-11-29 22:48:41 -0800

    Parse players.csv and register with the tournament controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { tournament } from "./fixture";
 import { PlayerList } from "./Views";
+import { FileUploader } from "react-drag-drop-files";
+import { parse } from "papaparse";
+import { TournamentController } from "./tournamentController";
+import { Registration, isRegistration } from "./model";
+
+const tournamentController = new TournamentController();
 
 function App() {
-  return <PlayerList players={tournament.players} />;
+  const handleChange = (file: File) => {
+    parse(file, {
+      header: true,
+      complete: function (results, file) {
+        for (const entry of results.data) {
+          if (!isRegistration(entry)) continue;
+          let name = entry["First Name"] + " " + entry["Last Name"];
+          tournamentController.registerPlayer(name, entry["Club"]);
+        }
+        console.log(tournamentController.players);
+      },
+    });
+  };
+  const types = ["csv"];
+  return (
+    <div>
+      <h1>Old School Tournament Runner</h1>
+      <FileUploader
+        handleChange={handleChange}
+        name="players.csv"
+        label="Upload or drop a players.csv file here"
+        types={types}
+      >
+        Put the players.csv file here
+      </FileUploader>
+    </div>
+  );
 }
 
 export default App;
--- src/model.ts
+++ src/model.ts
@@ -29,3 +29,18 @@ export interface Tournament {
   players: Player[];
   rounds: Round[];
 }
+
+export interface Registration {
+  "Last Name": string;
+  "First Name": string;
+  Club: string;
+}
+
+export function isRegistration(arg: any): arg is Registration {
+  return (
+    arg &&
+    typeof arg["Last Name"] === "string" &&
+    typeof arg["First Name"] === "string" &&
+    typeof arg["Club"] === "string"
+  );
+}
